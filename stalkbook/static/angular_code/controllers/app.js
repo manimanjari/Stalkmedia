ccf9317178a747e81ffd6624c0cdb41099721b8b
manimanjari <manimanjari25@gmail.com>
2016-06-13 10:31:23 +0530

    title display

--- stalkbook/static/angular_code/controllers/app.js
+++ stalkbook/static/angular_code/controllers/app.js
@@ -3,17 +3,23 @@ module.config(function ($interpolateProvider) {
     $interpolateProvider.startSymbol('[[').endSymbol(']]');
 })
 
+module.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.
+            otherwise({
+                redirectTo: '/'
+            });
+    }]);
+
 module.controller("TagController", function($scope, $http) {
 
     $scope.image = ""
     $scope.search = function(image) {
-        console.log($scope.image)
         $scope.tag = '/images/?q=' + $scope.image
         $http.get($scope.tag).success(function(data,status,headers,config){
-        $scope.tag_urls={}
+        $scope.tag_data={}
         for(i=0;i<data.length;i++){
-            $scope.tag_data=data[i];
-            $scope.tag_urls[i]=$scope.tag_data.url
+            $scope.tag_data[i]=data[i];
         }
       }).error(function(data,status,headers,config){
         })
