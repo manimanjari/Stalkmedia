4278202331c1230e70d56b9abdffa0dcd56abe05
manimanjari <manimanjari25@gmail.com>
2016-06-13 11:05:13 +0530

    made indentation changes

--- stalkbook/static/angular_code/controllers/app.js
+++ stalkbook/static/angular_code/controllers/app.js
@@ -16,13 +16,12 @@ module.controller("TagController", function($scope, $http) {
     $scope.image = ""
     $scope.search = function(image) {
         $scope.tag = '/images/?q=' + $scope.image
-        $http.get($scope.tag).success(function(data,status,headers,config){
-        $scope.tag_data={}
-        for(i=0;i<data.length;i++){
-            $scope.tag_data[i]=data[i];
+        $http.get($scope.tag).success(function(data, status, headers, config){
+        $scope.tag_data = {}
+        for(i=0; i<data.length; i++){
+            $scope.tag_data[i] = data[i];
         }
       }).error(function(data,status,headers,config){
-        })
+      })
     }
-
 });
