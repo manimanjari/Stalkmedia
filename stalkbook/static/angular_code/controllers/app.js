7a2c2560f0246ecfb10569a46af39c45c497ed54
manimanjari <manimanjari25@gmail.com>
2016-06-13 09:38:28 +0530

    picture display

--- stalkbook/static/angular_code/controllers/app.js
+++ stalkbook/static/angular_code/controllers/app.js
@@ -2,46 +2,21 @@ var module = angular.module("stalkmediaApp", ['ngRoute']);
 module.config(function ($interpolateProvider) {
     $interpolateProvider.startSymbol('[[').endSymbol(']]');
 })
-module.config(['$routeProvider',
-    function($routeProvider) {
-        $routeProvider.
-            when('/flickr', {
-                templateUrl: static_url + 'angular_code/partial_views/flickr_view.html',
-                controller: 'RouteController1'
-            }).
-            when('/instagram', {
-                templateUrl: static_url + 'angular_code/partial_views/insta_view.html',
-                controller: 'RouteController2'
-            }).
-            otherwise({
-                redirectTo: '/'
-            });
-    }]);
 
 module.controller("TagController", function($scope, $http) {
 
     $scope.image = ""
-    console.log($scope.image)
     $scope.search = function(image) {
-
         console.log($scope.image)
-        $http({
-          method: 'GET',
-          url: "http://127.0.0.1:8000/images/?q" + $scope.image
-          
-        }).then(function successCallback(response) {
-              console.log(response)
-          }, function errorCallback(response) {
-                alert("error");
-         });
-         console.log(url)
+        $scope.tag = '/images/?q=' + $scope.image
+        $http.get($scope.tag).success(function(data,status,headers,config){
+        $scope.tag_urls={}
+        for(i=0;i<data.length;i++){
+            $scope.tag_data=data[i];
+            $scope.tag_urls[i]=$scope.tag_data.url
+        }
+      }).error(function(data,status,headers,config){
+        })
+    }
 
-      }
-
-});
-module.controller("RouteController1", function($scope) {
-    $scope.test="flickr photos"
-});
-module.controller("RouteController2", function($scope) {
-    $scope.test="insta photos"
 });
