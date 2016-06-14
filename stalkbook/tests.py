07ff3fa46c4f2043459a5e142678c39a221e336a
manimanjari <manimanjari25@gmail.com>
2016-06-14 17:08:53 +0530

    added test cases

--- stalkbook/tests.py
+++ stalkbook/tests.py
@@ -5,5 +5,15 @@ class StalkbookViewsTestCase(TestCase):
     def test_index(self):
         resp = self.client.get('/home/')
         self.assertEqual(resp.status_code, 200)
+
+    def test_valid_request(self):       
+        resp = self.client.get('/images/?q=rome')
+        self.assertEqual(resp.status_code, 200)
+       
+    def test_bad_request(self):
+        resp = self.client.get('/images/?x=rome')
+        self.assertEqual(resp.status_code, 400)
+       
+    def test_page_not_found(self):
         resp = self.client.get('/images/$')
         self.assertEqual(resp.status_code, 404)
